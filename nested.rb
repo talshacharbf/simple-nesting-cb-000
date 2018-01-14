58e70777273bfd97b9a9a5eaf23e218e0ef79ecf
tal shachar <tal.shachar@buzzfeed.com>
2018-01-14 21:34:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-		 programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
+		 programmer_hash[:dennis_ritchie][:known_for] = "GUI"
 		 programmer_hash
 
 end
